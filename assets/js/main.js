4f7eef441f1bb794b37d0086694ac43438ba63f8
Aaron DeVera <aarondevera@Aarons-Air-3.home>
2014-12-07 19:16:29 -0500

    night mode added

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,15 +1,22 @@
-/*window.onload=function(){
-	var t = new Trianglify({cellsize: 40});
-	var pattern = t.generate(document.body.clientWidth,document.body.clientHeight);
-	document.body.setAttribute('style','background-image:'+pattern.dataUrl+';background-size: cover');
-}*/
-/*
-setTimeout(function(){
-   window.location.reload(1);
-}, 5000);*/
-
 $(document).ready(function(){
-	$("#posts ul li").click(function(){
-		$("#posts").blur();
-	})
+	var day = true;
+	$("#night").click(function(){
+
+		if (day==true){
+			$("body").removeClass("greyback");
+			$("body").addClass("plumback");
+			$("a").removeClass("blacktext");
+			$("a").addClass("whitetext");
+			day = false;
+		}
+
+		else if (day==false){
+			$("body").removeClass("plumback");
+			$("body").addClass("greyback");
+			$("a").removeClass("whitetext");
+			$("a").addClass("blacktext");
+			day = true;
+		}
+
+	});
 });
