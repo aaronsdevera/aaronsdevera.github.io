b67bb9f57c06837f2c4179a8141c4e15cc82a8d3
Aaron DeVera <aarondevera@Aarons-Air-3.home>
2014-12-07 19:20:55 -0500

    day night text change

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,6 +4,7 @@ $(document).ready(function(){
 
 		if (day==true){
 			$(this).empty();
+			$(this).text("click here to read in day mode");
 			$("body").removeClass("greyback");
 			$("body").addClass("plumback");
 			$("a").removeClass("blacktext");
@@ -12,6 +13,8 @@ $(document).ready(function(){
 		}
 
 		else if (day==false){
+			$(this).empty();
+			$(this).text("click here to read in night mode");
 			$("body").removeClass("plumback");
 			$("body").addClass("greyback");
 			$("a").removeClass("whitetext");
