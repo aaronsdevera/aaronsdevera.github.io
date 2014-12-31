a69b15fd709ce2c845b721fdd4f4bfead57a1ee8
aaronsdevera <aaronsdevera@gmail.com>
2014-12-30 19:26:35 -0800

    portfolio-6.0.0 + nyetscape

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,26 +1,50 @@
-$(document).ready(function(){
-	var day = true;
-	$("#night").click(function(){
 
-		if (day==true){
-			$(this).empty();
-			$(this).text("click here to read in day mode");
-			$("body").removeClass("greyback");
-			$("body").addClass("plumback");
-			$("a").removeClass("blacktext");
-			$("a").addClass("whitetext");
-			day = false;
+
+$(document).ready(function(){
+
+	$("#terminal").hide();
+	var terminalOn = false;
+
+
+	$('html').keydown(function(e){
+		if(e.keyCode == 192){
+			if (terminalOn == false){
+				$("#terminal").show();
+				terminalOn = true;
+			}
+
+			else if (terminalOn == true){
+				$("#terminal").hide();
+				terminalOn = false;
+			}
+
 		}
 
-		else if (day==false){
-			$(this).empty();
-			$(this).text("click here to read in night mode");
-			$("body").removeClass("plumback");
-			$("body").addClass("greyback");
-			$("a").removeClass("whitetext");
-			$("a").addClass("blacktext");
-			day = true;
+
+		else if(e.keyCode == 13){
+			var termInput = document.getElementById('in').value;
+			if (termInput == "night"){
+				console.log("Initiating night mode.");
+				$("body").removeClass("greyback");
+				$("body").addClass("plumback");
+				$("a").removeClass("blacktext");
+				$("a").addClass("whitetext");
+				$("nav").removeClass("blackborder");
+				$("nav").addClass("whiteborder");
+			}
+
+			else if (termInput == "day"){
+
+				$("body").removeClass("plumback");
+				$("body").addClass("greyback");
+				$("a").removeClass("whitetext");
+				$("a").addClass("blacktext");
+				$("nav").removeClass("whiteborder");
+				$("nav").addClass("blackborder");
+			}
 		}
 
 	});
+
+
 });
